a51997c54dc5a1327d99106bc6aa5d0fdcf11f03
danielaescutia07 <danielaescutia@yahoo.com>
2021-08-13 18:40:14 -0500

    final commit

--- README.md
+++ README.md
@@ -23,11 +23,15 @@ Commit your code regularly and meaningfully.
 Please answer the following questions below, you may edit the readme file to include your answers below the question.
 
 1. How would you describe acessibility on the web to someone new to programming?
+    Allowing all people to interact on the web.
 2. Talk about 3 different things you can do to ensure your website is accessible. 
+    Make sure you are using semantic HTML, alt attributes are filled out and using responsive units.
 3. How would you explain the concept of a variable to someone new to programming?
+    It's like a container that stores data.
 4. What is the purpose of using functions in code?
+    It allows you to process data and reuse it as many times as you want.
 5. How do you access a key inside of an object inside of an array?
-
+    array[object].value
 You are expected to be able to answer questions in these areas. Your responses contribute to your Sprint Challenge grade. 
 
 ## Instructions
--- index.js
+++ index.js
@@ -208,17 +208,17 @@ Practice accessing data above by console.log-ing following items:
 (no functions needed) */
 
 //(1) Name of the first artist (0th index) in the array
-
+console.log('Task 1:', artists[0].name);
 
 //(2) Bio of the third artist (2nd index) in the array 
-
+console.log('Task 1(2):', artists[2].bio);
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 (no function needed) 
 There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
-
-
+artists[9].name = "Vincent Van Gogh";
+console.log('Task 2:', artists);
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀  
 Use getArtistByIndex to do the following:
@@ -228,10 +228,10 @@ Use getArtistByIndex to do the following:
 
 Example, if getArtistByIndex is invoked with the artists array and the number 0, it will return `the artist at index 0 is Amedeo Modigliani` */
 
-function getArtistByIndex(/*Your Code Here*/) {
-  /*Your Code Here*/
+function getArtistByIndex(array,number) {
+  return `the artist at index ${array[number]['id']} is ${array[number]['name']}`;
 }
-
+  console.log('Task: 3', getArtistByIndex(artists,0));
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
@@ -243,11 +243,16 @@ Example born in 1901 and died in 1959 - included -- born in 1889 and died in 192
 If correct, the function should return ["Salvador Dali", "Frida Kahlo"]*/
 // Hint - Look up the .split() method
 
-function get20s(/*Your Code Here*/) {
-  /*Your Code Here*/
+ function get20s(array) {
+  const year = [];
+  for(let i = 0; i < array.length; i++){
+    if(array[i]['years'] >= '1900' && array[i]['years'] <= '2000'){
+      year.push(array[i].name);
+    }
+  }
+  return year;
 }
-
-
+  console.log('Task 4:', get20s(artists));
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Use removeArtist to do the following:
@@ -258,10 +263,11 @@ Use removeArtist to do the following:
 
 For example, if removeArtist is invoked with the artists array and the number 0, it will remove Amedeo Modigliani from our dataset and return the number 19. */
 
-function removeArtist(/*Your Code Here*/) {
-  /*Your Code Here*/
+function removeArtist(array,number) {
+  array.splice(number,1); 
+  return array.length;
 }
-
+console.log('Task 5:', removeArtist(artists,0));
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Use addArtist to do the following: 
@@ -279,10 +285,11 @@ Use addArtist to do the following:
 
 Example: addArtist(artists) should return the artists array with the above object added to the end of the array. */
 
-function addArtist(/*Your Code Here*/) {
-  /*Your Code Here*/
+function addArtist(array, id, name, years, genre, nationality, bio) {
+  array.push({id, name, years, genre, nationality, bio});
+  return array;
 }
-
+  console.log('Task 6:', addArtist(artists, 20, 'Daniela', '1988', 'Web Design', 'Mexican', 'Born in Mexico and moved to U.S. at an early age. She experienced the difference between two countries. Overcoming obstacles became a hobby and currently facing the biggest one... BECOMING THE BEST VERSION OF HERSELF.'));
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
@@ -292,10 +299,16 @@ Use lotsOfArt to do the following:
 
 For example lotsOfArt(artists); will return ["Amedeo Modigliani", "Rene Magritte", ... "Albrecht Dürer"]*/
 
-function lotsOfArt(/*Your Code Here*/) {
-  /*Your Code Here*/
+function lotsOfArt(array) {
+  const painting = []
+  for(let i = 0; i < array.length; i++){
+    if(array[i].paintings > 100){
+      painting.push(array[i].name);
+    }
+  }
+  return painting;
 }
-
+  console.log('Task 7:', lotsOfArt(artists));
 
 /* ***** END OF TASKS ***** */
 
